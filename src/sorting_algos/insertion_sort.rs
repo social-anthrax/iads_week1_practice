dc14b563a8f6818a3f81f60066245cd6f7ac18d7
Artemis Livingstone <ar.d.livingstone@gmail.com>
2021-09-25 22:03:05 +0100

    fixed sort1

--- src/sorting_algos/insertion_sort.rs
+++ src/sorting_algos/insertion_sort.rs
@@ -1,5 +1,20 @@
 pub fn sort<T: PartialOrd + Copy + Ord>(a: &mut Vec<T>) {
     for i in 1..a.len() {
+        let x = a[i];
+
+        let mut loc = i; //don't decrease by one as last line doesn't increase.
+        for j in (0..i).rev() {
+            if a[j] <= x {break;}
+            
+            a[(j + 1)] = a[j];
+            loc = j;
+        }
+        a[loc] = x;
+    }
+}
+
+pub fn sort2<T: PartialOrd + Copy + Ord>(a: &mut Vec<T>) {
+    for i in 1..a.len() {
         let x = a[i]; // Copy value here
         let mut j: isize = (i - 1) as isize;
 
@@ -14,14 +29,18 @@ pub fn sort<T: PartialOrd + Copy + Ord>(a: &mut Vec<T>) {
 
 #[cfg(test)]
 mod test {
-    use crate::sorting_algos::insertion_sort::sort;
+    use crate::sorting_algos::insertion_sort::{sort, sort2};
 
     #[test]
     fn check_insert() {
         let mut array = vec![3, 2, 6, 8, 2, 4, 5, 9, 1, 2, 3, 4, 9, 7];
-        sort(&mut array);
         let mut new_array = array.to_vec();
+        let mut new_array2 = new_array.to_vec();
+        sort(&mut array);
+        sort2(&mut new_array2);
         new_array.sort();
+
         assert_eq!(array, *new_array);
+        assert_eq!(new_array, new_array2);
     }
 }
